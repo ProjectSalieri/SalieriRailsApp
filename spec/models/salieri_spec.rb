a6b2d91c613c02966ed006aa040a2f7b1890e805
Masakaze <redkingslime@gmail.com>
2016-01-10 17:39:14 +0900

    テスト修正

--- spec/models/salieri_spec.rb
+++ spec/models/salieri_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Salieri do
   it 'parse_for_genre_categorize' do
-    document = "私はサッカーが好きです。ボールを蹴りながら走るとスッキリします"
+    document = "私はサッカーが好きです。ボールを蹴りながら走ると楽しいし、スッキリします"
 
     salieri = Salieri.new()
     ret = salieri.parse_for_genre_categorize(document)
