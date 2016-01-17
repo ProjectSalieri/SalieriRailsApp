87ba068337fbf9b12871e640007f3e11a89f3f5a
Masakaze <redkingslime@gmail.com>
2016-01-17 22:27:27 +0900

    select_tagでカテゴリを選択できるよう設置

--- app/models/doc_category.rb
+++ app/models/doc_category.rb
@@ -2,4 +2,6 @@ class DocCategory < ActiveRecord::Base
 
   belongs_to :doc_category_type
   has_many :doc_category_infos
+
+  scope :where_type_genre, -> { where(:doc_category_type_id => DocCategoryType.type_genre.id) }
 end
