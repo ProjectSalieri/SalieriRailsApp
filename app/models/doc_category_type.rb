1b9c90271994b49831c02869785bbfb2ebfa7346
Masakaze <redkingslime@gmail.com>
2016-01-17 11:26:35 +0900

    MicrosoftNewCrawlerのモジュール名変更に対応.DocCategoryTypeを整理

--- app/models/doc_category_type.rb
+++ app/models/doc_category_type.rb
@@ -2,4 +2,7 @@ class DocCategoryType < ActiveRecord::Base
 
   has_many :words
   has_many :doc_categories
+
+  def self.type_genre ; 	return DocCategoryType.find_by({:name_en => "Genre"}) 	; end
+  def self.type_emotion ; 	return DocCategoryType.find_by({:name_en => "Emotion"}) ; end
 end
