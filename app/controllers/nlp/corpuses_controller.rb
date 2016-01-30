9cd5095131b97362081b36fb9c6f72017da687b8
Masakaze <redkingslime@gmail.com>
2016-01-31 02:01:55 +0900

    コーパス一覧ページの表示にかなりのSQL発行があったので修正

--- app/controllers/nlp/corpuses_controller.rb
+++ app/controllers/nlp/corpuses_controller.rb
@@ -1,11 +1,11 @@
 class Nlp::CorpusesController < ApplicationController
 
   def index
-    @word_names = Word.all.pluck(:name).uniq()
     @doc_category_type = DocCategoryType.type_genre
     if params.key?(:doc_catetory_type)
       @doc_category_type = DocCategoryType.find_by({:name_en => params[:doc_catetory_type]})
     end
+    @words = Word.where(:doc_category_type_id => @doc_category_type.id).includes(:doc_category_infos)
   end
 
 end
