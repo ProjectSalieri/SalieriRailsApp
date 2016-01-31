804ee1ab0b38ec0454efc4879c60049deed3cf64
Masakaze <redkingslime@gmail.com>
2016-01-31 23:22:23 +0900

    pluckの複数指定で改善 + 条件ミス修正

--- app/models/doc_category_info.rb
+++ app/models/doc_category_info.rb
@@ -9,20 +9,21 @@ class DocCategoryInfo < ActiveRecord::Base
 
   # 出現カウントの更新
   def self.update_appear_counts(category_type_id, category_id, word_names)
-    words = Word.where(:doc_category_type_id => category_type_id).where("name in (#{word_names.map{ |w| "\'#{w}\'" }.join(',')})").all
+    word_infos = Word.where(:doc_category_type_id => category_type_id).where("name in (#{word_names.map{ |w| "\'#{w}\'" }.join(',')})").pluck(:id, :name)
     category_ids = DocCategory.where(:doc_category_type_id => category_type_id).pluck(:id)
 
-    words.each { |word|
-      next if word_names.include?(word.name)
+    word_infos.each { |word_info|
+      word_name = word_info[1]
+      next unless word_names.include?(word_name)
 
       # 他のカテゴリのDocCategoryInfoがなければ一緒に生成
       update_info = nil
       category_ids.each { |cat_id|
-        info = DocCategoryInfo.find_or_create_by({doc_category_id: cat_id, word_id: word.id})
+        info = DocCategoryInfo.find_or_create_by({doc_category_id: cat_id, word_id: word_info[0]})
         update_info = info if category_id == cat_id
       }
     
-      update_info.appear_count += word_names.count(word.name)
+      update_info.appear_count += word_names.count(word_name)
       update_info.save
     }
   end
