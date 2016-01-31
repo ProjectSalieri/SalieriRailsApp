9aeac3b98f60207958d6c7ec1be66c00c28b7169
Masakaze <redkingslime@gmail.com>
2016-02-01 00:51:13 +0900

    コーパス一覧のコントローラースペルミス修正

--- app/controllers/nlp/corpuses_controller.rb
+++ app/controllers/nlp/corpuses_controller.rb
@@ -2,8 +2,8 @@ class Nlp::CorpusesController < ApplicationController
 
   def index
     @doc_category_type = DocCategoryType.type_genre
-    if params.key?(:doc_catetory_type)
-      @doc_category_type = DocCategoryType.find_by({:name_en => params[:doc_catetory_type]})
+    if params.key?(:doc_category_type)
+      @doc_category_type = DocCategoryType.find_by({:name_en => params[:doc_category_type]})
     end
     @words = Word.where(:doc_category_type_id => @doc_category_type.id).includes(:doc_category_infos)
   end
